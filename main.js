358f627963a3d3237253a8b40465c390eaa6d5b3
Bachir Aouad <bachiraouad.aouad@gmail.com>
2022-03-24 11:27:46 +0100

    Class Arme
    Ezwin

---
+++ main.js
@@ -0,0 +1,51 @@
+class Arme {
+    
+    _name;
+    _str;
+    _end;
+    _gold;
+
+    constructor(name,str, end, gold){
+        this._name = name;
+        this._str = str;
+        this._end = end;
+        this._gold = gold;
+    }
+}
+
+const Arme1 = new Arme ('Epee',1,0,50)
+console.log(Arme1);
+
+const Arme2 = new Arme ('Fouet',1,1,100)
+console.log(Arme2);
+
+const Arme3 = new Arme ('Hache',3,2,200)
+console.log(Arme3);
+
+const Arme4 = new Arme  ('Masse',5,3,300)
+console.log(Arme4);
+
+const Arme5 = new Arme  ('Baton',10,10,500)
+console.log(Arme5);
+
+const Arme6 = new Arme  ('Rapier',15,13,750)
+console.log(Arme6);
+
+const Arme7 = new Arme  ('Katana',20,17,1000)
+console.log(Arme7);
+
+const Arme8 = new Arme  ('Dague',25,20,1500)
+console.log(Arme8);
+
+const Arme9 = new Arme ('Lance',30,24,2000)
+console.log(Arme9);
+
+const Arme10 = new Arme ("Great Sword",35,28,2500)
+console.log(Arme10);
+
+const Arme11 = new Arme ("Ushigatana",40,32,3000)
+console.log(Arme11);
+
+const Arme12 = new Arme ("Excalibur",100,100,9999)
+console.log(Arme12);
+
